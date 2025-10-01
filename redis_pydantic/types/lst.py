6c8fcc3f002551bdc86af3f88b07d8f82d47c22c
YedidyaHKfir <Yedidya@dono.ai>
2025-10-01 12:02:21 +0300

    created append function for redis list

--- redis_pydantic/types/lst.py
+++ redis_pydantic/types/lst.py
@@ -1,18 +1,23 @@
+from redis.asyncio.client import Redis
+
 from redis_pydantic.types.base import RedisType
 from redis_pydantic.context import _context_var
 
 
 class RedisList(list, RedisType):
-    def __init__(self, *args, redis_key: str, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, redis_key: str, field_path: str, redis: Redis, **kwargs):
+        super(list, self).__init__(*args, **kwargs)
         self.redis_key = redis_key
+        self.field_path = field_path
+        self.redis = redis
 
     def load(self):
         pass
 
     def append(self, __object):
-        redis = _context_var.get()
-        redis.rpush(self.redis_key, __object)
+        redis = _context_var.get() or self.redis
+        redis.json().arrappend(self.redis_key, self.field_path, __object)
+        super(list, self).append(__object)
 
     def extend(self, __iterable):
         pass
