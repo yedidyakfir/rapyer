790de31d11452130a1da4b079e717c347d935198
YedidyaHKfir <Yedidya@dono.ai>
2025-10-03 00:05:16 +0300

    created implementation for redis dcit

--- redis_pydantic/types/dct.py
+++ redis_pydantic/types/dct.py
@@ -3,25 +3,63 @@ from redis_pydantic.types.base import RedisType
 
 class RedisDict(dict, RedisType):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        RedisType.__init__(self, **kwargs)
+        super().__init__(*args)
 
     async def load(self):
-        pass
+        # Get all items from Redis dict
+        redis_items = await self.pipeline.json().get(self.redis_key, self.field_path)
+
+        if redis_items is None:
+            redis_items = {}
+
+        # Clear local dict and populate with Redis data
+        super().clear()
+        super().update(redis_items)
 
     def __setitem__(self, key, value):
-        pass
+        super().__setitem__(key, value)
+
+        return self.pipeline.json().set(
+            self.redis_key, f"{self.json_path}.{key}", str(value)
+        )
 
     def __delitem__(self, key):
-        pass
+        super().__delitem__(key)
 
-    def update(self, *args, **kwargs):
-        pass
+        return self.pipeline.json().delete(self.redis_key, f"{self.json_path}.{key}")
+
+    def update(self, **kwargs):
+        super().update(kwargs)
+        redis_kwargs = {f"{self.json_path}.{k}": v for k, v in kwargs.items()}
+        return self.pipeline.json().mset(
+            [
+                (self.redis_key, field_name, value)
+                for field_name, value in redis_kwargs.items()
+            ]
+        )
 
     def pop(self, key, default=None):
-        pass
+        if key in self:
+            value = super().pop(key)
+            self.pipeline.json().delete(self.redis_key, f"{self.json_path}.{key}")
+            return value
+        elif default is not None:
+            return default
+        else:
+            raise KeyError(key)
 
     def popitem(self):
-        pass
+        key, value = super().popitem()
+        self.pipeline.json().delete(self.redis_key, f"{self.json_path}.{key}")
+        return key, value
 
     def clear(self):
-        pass
+        # Clear local dict
+        super().clear()
+
+        # Clear Redis dict
+        return self.pipeline.json().delete(self.redis_key, self.json_path)
+
+    def clone(self):
+        return dict.copy(self)
