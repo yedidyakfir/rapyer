ffe6f30b2543ec7a68cbb05e153822e1b3b261d7
YedidyaHKfir <Yedidya@dono.ai>
2025-10-08 12:32:56 +0300

    use also python 3.10

--- redis_pydantic/config.py
+++
@@ -1,8 +0,0 @@
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix="REDIS_PYDANTIC",
-    settings_files=["settings.toml", ".secrets.toml"],
-    environments=True,
-    load_dotenv=True,
-)
