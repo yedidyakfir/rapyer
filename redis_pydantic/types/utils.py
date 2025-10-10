4a03466b9ef26395bb9fc578ab2fa7fe482b29d7
YedidyaHKfir <Yedidya@dono.ai>
2025-10-10 13:52:30 +0300

    update names

--- redis_pydantic/types/utils.py
+++ redis_pydantic/types/utils.py
@@ -3,5 +3,5 @@ async def noop():
 
 
 def update_keys_in_pipeline(pipeline, redis_key: str, **kwargs):
-    for key, value in kwargs.items():
-        pipeline.json().set(redis_key, key, value)
+    for json_path, value in kwargs.items():
+        pipeline.json().set(redis_key, json_path, value)
