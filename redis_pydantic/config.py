f459eab96bfcd62d189daf8b4cbcf12815522cd9
YedidyaHKfir <Yedidya@dono.ai>
2025-10-10 17:04:34 +0300

    prevent recursive import

--- redis_pydantic/config.py
+++ redis_pydantic/config.py
@@ -3,9 +3,14 @@ import dataclasses
 import redis
 from redis.asyncio import Redis
 
-from redis_pydantic.types import ALL_TYPES
 
 DEFAULT_CONNECTION = "redis://localhost:6379/0"
+
+
+def create_all_types():
+    from redis_pydantic.types import ALL_TYPES
+
+    return ALL_TYPES
 
 
 @dataclasses.dataclass
@@ -13,7 +18,7 @@ class RedisConfig:
     redis: Redis = dataclasses.field(
         default_factory=lambda: redis.asyncio.from_url(DEFAULT_CONNECTION)
     )
-    redis_type: dict[str, type] = dataclasses.field(default_factory=lambda: ALL_TYPES)
+    redis_type: dict[str, type] = dataclasses.field(default_factory=create_all_types)
     ttl: int | None = None
 
 
