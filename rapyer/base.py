2c12906028ca6614e44d55f323f140ab820f6bfd
YedidyaHKfir <Yedidya@dono.ai>
2025-10-22 22:58:04 +0300

    [switch_annotation] - allow non default fields

--- rapyer/base.py
+++ rapyer/base.py
@@ -5,7 +5,7 @@ import functools
 import uuid
 from typing import Self, ClassVar, Any
 
-from pydantic import BaseModel, PrivateAttr
+from pydantic import BaseModel, PrivateAttr, ConfigDict
 from pydantic.fields import FieldInfo
 from pydantic_core import PydanticUndefined
 
@@ -27,6 +27,7 @@ class AtomicRedisModel(BaseModel):
     Meta: ClassVar[RedisConfig] = RedisConfig()
     field_config: ClassVar[RedisFieldConfig] = RedisFieldConfig()
     _field_config_override: RedisFieldConfig = None
+    model_config = ConfigDict(validate_assignment=True)
 
     @property
     def pk(self):
@@ -73,7 +74,7 @@ class AtomicRedisModel(BaseModel):
         for attr_name, attr_type in cls.__annotations__.items():
             if original_annotations[attr_name] == attr_type:
                 continue
-            value = getattr(cls, attr_name)
+            value = getattr(cls, attr_name, None)
             if value is None:
                 continue
 
@@ -214,21 +215,21 @@ class AtomicRedisModel(BaseModel):
             _context_var.set(None)
             _context_xx_pipe.set(False)
 
-    def __setattr__(self, name: str, value: Any) -> None:
-        if value is None:
-            super().__setattr__(name, value)
-            return
-
-        is_already_at_correct_type = isinstance(value, (RedisType, AtomicRedisModel))
-        if name in self.model_fields and not is_already_at_correct_type:
-            field_info = self.model_fields[name]
-            field_type = field_info.annotation
-
-            if safe_issubclass(field_type, RedisType):
-                value = field_type.from_orig(value)
-
-            # Set the converted Redis instance
-            super().__setattr__(name, value)
-        else:
-            # Use the parent's __setattr__ for all other cases
-            super().__setattr__(name, value)
+    # def __setattr__(self, name: str, value: Any) -> None:
+    #     if value is None:
+    #         super().__setattr__(name, value)
+    #         return
+    #
+    #     is_already_at_correct_type = isinstance(value, (RedisType, AtomicRedisModel))
+    #     if name in self.model_fields and not is_already_at_correct_type:
+    #         field_info = self.model_fields[name]
+    #         field_type = field_info.annotation
+    #
+    #         if safe_issubclass(field_type, RedisType):
+    #             value = field_type.from_orig(value)
+    #
+    #         # Set the converted Redis instance
+    #         super().__setattr__(name, value)
+    #     else:
+    #         # Use the parent's __setattr__ for all other cases
+    #         super().__setattr__(name, value)
