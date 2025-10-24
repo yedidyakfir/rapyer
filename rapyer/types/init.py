5f1c2224afe668eb6cc23881f065defa444b1919
YedidyaHKfir <Yedidya@dono.ai>
2025-10-24 16:34:47 +0300

    [switch_annotation] - we dont use serializers

--- rapyer/types/init.py
+++ rapyer/types/init.py
@@ -1,21 +1,12 @@
 from datetime import datetime
-from typing import get_origin
 
-from pydantic import BaseModel
-
-from rapyer.types.base import PydanicSerializer
-from rapyer.types.boolean import BooleanSerializer
 from rapyer.types.boolean import RedisBool
-from rapyer.types.byte import ByteSerializer
 from rapyer.types.byte import RedisBytes
-from rapyer.types.datetime import DatetimeSerializer
 from rapyer.types.datetime import RedisDatetime
-from rapyer.types.dct import DictSerializer
 from rapyer.types.dct import RedisDict
 from rapyer.types.integer import RedisInt
 from rapyer.types.lst import RedisList
 from rapyer.types.string import RedisStr
-from rapyer.types.string import StringSerializer
 
 ALL_TYPES = {
     list: RedisList,
@@ -26,21 +17,3 @@ ALL_TYPES = {
     str: RedisStr,
     datetime: RedisDatetime,
 }
-
-SERIALIZER = {
-    dict: DictSerializer,
-    bytes: ByteSerializer,
-    bool: BooleanSerializer,
-    str: StringSerializer,
-    datetime: DatetimeSerializer,
-}
-
-
-def create_serializer(type_annotation):
-    serializer_creator = create_serializer
-
-    origin_type = get_origin(type_annotation) or type_annotation
-    if issubclass(origin_type, BaseModel):
-        return PydanicSerializer(type_annotation, serializer_creator)
-    serializer_type = SERIALIZER.get(origin_type, origin_type)
-    return serializer_type(type_annotation, serializer_creator)
