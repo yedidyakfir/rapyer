2c5ee9e0ff5b7d96149fe8b42fc22ae7d77ce2c3
YedidyaHKfir <Yedidya@dono.ai>
2025-10-26 22:39:45 +0200

    [switch_annotation] - update bool types

--- rapyer/types/boolean.py
+++ rapyer/types/boolean.py
@@ -1,39 +1,18 @@
-from rapyer.types.base import RedisType, RedisSerializer
-
-
-class BooleanSerializer(RedisSerializer):
-    def serialize_value(self, value):
-        return bool(value) if value is not None else False
-
-    def deserialize_value(self, value):
-        if isinstance(value, bool):
-            return value
-        elif isinstance(value, (int, float)):
-            return bool(value)
-        elif isinstance(value, str):
-            return value.lower() in ("true", "1")
-        else:
-            return bool(value)
+from rapyer.types.base import RedisType
 
 
 class RedisBool(int, RedisType):
-    serializer = BooleanSerializer(bool, None)
     original_type = bool
 
     async def load(self):
         redis_value = await self.client.json().get(self.redis_key, self.field_path)
-        if redis_value is not None:
-            return self.serializer.deserialize_value(redis_value)
-        return False
+        return redis_value
 
     async def set(self, value: bool):
         if not isinstance(value, bool):
             raise TypeError("Value must be bool")
 
-        serialized_value = self.serializer.serialize_value(value)
-        return await self.client.json().set(
-            self.redis_key, self.json_path, serialized_value
-        )
+        return await self.client.json().set(self.redis_key, self.json_path, value)
 
     def clone(self):
         return bool(self)
