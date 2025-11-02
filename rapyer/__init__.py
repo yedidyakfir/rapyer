a5811370aad02a7c5501f0bc01a1f15801115d9e
YedidyaHKfir <Yedidya@dono.ai>
2025-11-02 18:07:06 +0200

    added base tyeps to init

--- rapyer/__init__.py
+++ rapyer/__init__.py
@@ -1,6 +1,18 @@
 """Redis Pydantic - Pydantic models with Redis as the backend."""
 
+from rapyer.types.byte import RedisBytes
+from rapyer.types.dct import RedisDict
+from rapyer.types.integer import RedisInt
+from rapyer.types.lst import RedisList
+from rapyer.types.string import RedisStr
 from .base import AtomicRedisModel
 
 __version__ = "0.1.0"
-__all__ = ["AtomicRedisModel"]
+__all__ = [
+    "AtomicRedisModel",
+    "RedisStr",
+    "RedisInt",
+    "RedisBytes",
+    "RedisList",
+    "RedisDict",
+]
