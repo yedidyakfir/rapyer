d797c9cbe4f8f101d27e7e42e73c9080dae12f87
yedidyakfir <yedidyakfir@gmail.com>
2025-11-05 17:10:02 +0200

    [enable_inheritance_models] - update redis or ttl only if it is not None

--- rapyer/init.py
+++ rapyer/init.py
@@ -4,10 +4,12 @@ from redis.asyncio.client import Redis
 from rapyer.base import REDIS_MODELS
 
 
-def init_rapyer(redis: str | Redis, ttl: int = None):
+def init_rapyer(redis: str | Redis = None, ttl: int = None):
     if isinstance(redis, str):
         redis = redis_async.from_url(redis)
 
     for model in REDIS_MODELS:
-        model.Meta.redis = redis
-        model.Meta.ttl = ttl
+        if redis is not None:
+            model.Meta.redis = redis
+        if ttl is not None:
+            model.Meta.ttl = ttl
