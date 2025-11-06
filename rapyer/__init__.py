a192dbcfe78ff7d55857430fc96818704044100a
yedidyakfir <yedidyakfir@gmail.com>
2025-11-06 20:24:02 +0200

    [add-aupdate-to-base-model] - black

--- rapyer/__init__.py
+++ rapyer/__init__.py
@@ -1,12 +1,12 @@
 """Redis Pydantic - Pydantic models with Redis as the backend."""
 
+from rapyer.base import AtomicRedisModel
 from rapyer.init import init_rapyer
 from rapyer.types.byte import RedisBytes
 from rapyer.types.dct import RedisDict
 from rapyer.types.integer import RedisInt
 from rapyer.types.lst import RedisList
 from rapyer.types.string import RedisStr
-from rapyer.base import AtomicRedisModel
 
 __version__ = "0.1.0"
 __all__ = [
