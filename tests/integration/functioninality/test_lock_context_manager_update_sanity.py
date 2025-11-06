a6eeb39a7f35c6257fe04d9522e7bc243235fa89
yedidyakfir <yedidyakfir@gmail.com>
2025-11-06 19:56:43 +0200

    [add-aupdate-to-base-model] - added check that the model preserves redis types in the context

--- tests/integration/functioninality/test_lock_context_manager_update_sanity.py
+++ tests/integration/functioninality/test_lock_context_manager_update_sanity.py
@@ -1,5 +1,6 @@
 import pytest
 
+from rapyer import RedisStr, RedisInt, RedisList
 from tests.models.functionality_types import LockUpdateTestModel as LockTestModel
 
 
@@ -18,6 +19,17 @@ async def test_lock_context_manager_updates_model_with_new_data_sanity():
 
     # Assert
     async with original_model.lock() as locked_model:
+        assert isinstance(original_model.name, RedisStr)
+        assert isinstance(locked_model.name, RedisStr)
+        assert locked_model.name.json_path == original_model.name.json_path == "$.name"
         assert locked_model.name == "updated_name" == original_model.name
+        assert isinstance(original_model.value, RedisInt)
+        assert isinstance(locked_model.value, RedisInt)
+        assert (
+            locked_model.value.json_path == original_model.value.json_path == "$.value"
+        )
         assert locked_model.value == 100 == original_model.value
+        assert isinstance(original_model.tags, RedisList)
+        assert isinstance(locked_model.tags, RedisList)
+        assert locked_model.tags.json_path == original_model.tags.json_path == "$.tags"
         assert locked_model.tags == ["tag1", "tag2"] == original_model.tags
