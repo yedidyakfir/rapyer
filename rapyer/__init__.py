bd0bbcd5dc196f84212984a55b9ef2500743c735
yedidyakfir <yedidyakfir@gmail.com>
2025-11-08 19:13:24 +0200

    [support-more-versions] - change the imports

--- rapyer/__init__.py
+++ rapyer/__init__.py
@@ -2,20 +2,9 @@
 
 from rapyer.base import AtomicRedisModel
 from rapyer.init import init_rapyer, teardown_rapyer
-from rapyer.types.byte import RedisBytes
-from rapyer.types.dct import RedisDict
-from rapyer.types.integer import RedisInt
-from rapyer.types.lst import RedisList
-from rapyer.types.string import RedisStr
 
-__version__ = "0.1.0"
 __all__ = [
     "AtomicRedisModel",
-    "RedisStr",
-    "RedisInt",
-    "RedisBytes",
-    "RedisList",
-    "RedisDict",
     "init_rapyer",
     "teardown_rapyer",
 ]
--- rapyer/types/__init__.py
+++ rapyer/types/__init__.py
@@ -1,4 +1,5 @@
 from rapyer.types.byte import RedisBytes
+from rapyer.types.datetime import RedisDatetime
 from rapyer.types.dct import RedisDict
 from rapyer.types.integer import RedisInt
 from rapyer.types.lst import RedisList
@@ -11,4 +12,5 @@ __all__ = [
     "RedisBytes",
     "RedisList",
     "RedisDict",
+    "RedisDatetime",
 ]
