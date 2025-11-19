921c6f3dfef38c01ff02505a3e4a68088817a071
YedidyaHKfir <Yedidya@dono.ai>
2025-11-19 17:41:11 +0200

    [support-all-pickable-in-list-dict] - [support-all-pickable-in-list-dict] - added test to check if they can pickle other types

---
+++ tests/integration/dct/test_redis_dict_pickable_types.py
@@ -0,0 +1,47 @@
+import pytest
+from typing import Type
+
+from rapyer import RedisStr
+from rapyer.types.dct import RedisDict
+from tests.models.pickle_types import DictPickableTypesModel
+
+
+@pytest.mark.parametrize(
+    ["type_data", "tuple_data", "set_data", "frozenset_data"],
+    [
+        [
+            {"type1": str},
+            {"tuple1": ("hello", 42), "tuple2": ("world", 100)},
+            {"set1": {"a", "b"}, "set2": {"x", "y", "z"}},
+            {"fs1": frozenset([1, 2, 3]), "fs2": frozenset([4, 5])},
+        ],
+        [
+            {"type1": RedisStr},
+            {"tuple1": ("single", 1)},
+            {"set1": {"one"}},
+            {"fs1": frozenset([42])},
+        ],
+    ],
+)
+@pytest.mark.asyncio
+async def test_redis_dict_pickable_types_save_load_sanity(
+    type_data, tuple_data, set_data, frozenset_data
+):
+    # Arrange
+    model = DictPickableTypesModel(
+        type_dict=type_data,
+        tuple_dict=tuple_data,
+        set_dict=set_data,
+        frozenset_dict=frozenset_data,
+    )
+
+    # Act
+    await model.save()
+    loaded_model = await DictPickableTypesModel.get(model.key)
+
+    # Assert
+    assert model == loaded_model
+    assert loaded_model.type_dict == type_data
+    assert loaded_model.tuple_dict == tuple_data
+    assert loaded_model.set_dict == set_data
+    assert loaded_model.frozenset_dict == frozenset_data
---
+++ tests/integration/lst/test_redis_list_pickable_types.py
@@ -0,0 +1,42 @@
+import pytest
+
+from tests.models.pickle_types import ListPickableTypesModel
+
+
+@pytest.mark.parametrize(
+    ["type_data", "tuple_data", "set_data", "frozenset_data", "nested_list"],
+    [
+        [
+            [str, int, list],
+            [("hello", 42), ("world", 100), ("test", 1)],
+            [{"a", "b"}, {"x", "y", "z"}],
+            [frozenset([1, 2, 3]), frozenset([4, 5])],
+            [],
+        ],
+        [[str, type], [("single", 1)], [{"one"}], [frozenset([42])], [[str, int]]],
+    ],
+)
+@pytest.mark.asyncio
+async def test_redis_list_pickable_types_save_load_sanity(
+    type_data, tuple_data, set_data, frozenset_data, nested_list
+):
+    # Arrange
+    model = ListPickableTypesModel(
+        type_list=type_data,
+        tuple_list=tuple_data,
+        set_list=set_data,
+        frozenset_list=frozenset_data,
+        nested_list=nested_list,
+    )
+
+    # Act
+    await model.save()
+    loaded_model = await ListPickableTypesModel.get(model.key)
+
+    # Assert
+    assert model == loaded_model
+    assert loaded_model.type_list == type_data
+    assert loaded_model.tuple_list == tuple_data
+    assert loaded_model.set_list == set_data
+    assert loaded_model.frozenset_list == frozenset_data
+    assert loaded_model.nested_list == nested_list
