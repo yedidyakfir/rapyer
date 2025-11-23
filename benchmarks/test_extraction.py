2f5a82794f15d5a9cd901091b7025a8f9cd183d7
YedidyaHKfir <Yedidya@dono.ai>
2025-11-23 13:08:16 +0200

    [add-benchmarking] - tries more thinks for extraction benchamrk

--- benchmarks/test_extraction.py
+++ benchmarks/test_extraction.py
@@ -1,6 +1,10 @@
 import asyncio
+from datetime import datetime
+
 import pytest
 import pytest_asyncio
+
+from rapyer import AtomicRedisModel
 from tests.models.simple_types import UserModelWithoutTTL
 
 COMPARE_FULL_EXTRACTION_NAME = "COMPARE_FULL_EXTRACTION_NAME"
@@ -12,26 +16,26 @@ async def setup_test_data():
     models = [UserModelWithoutTTL(name=f"user_{i}", age=20 + i) for i in range(100)]
     await asyncio.gather(*[model.save() for model in models])
     yield models
-    # Cleanup
-    keys = await UserModelWithoutTTL.afind_keys()
-    if keys:
-        await asyncio.gather(*[UserModelWithoutTTL.delete_by_key(key) for key in keys])
+
+
+async def extract_each_key(redis_klass: type[AtomicRedisModel]):
+    start = datetime.now()
+    keys = await redis_klass.afind_keys()
+    res = await asyncio.gather(*[redis_klass.get(key) for key in keys])
+    end = datetime.now()
+    delta = end - start
+    print(delta)
+
+
+async def afind_extract(redis_klass: type[AtomicRedisModel]):
+    start = datetime.now()
+    res = await redis_klass.afind()
+    end = datetime.now()
+    delta = end - start
+    print(delta)
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "setup_benchmark", [COMPARE_FULL_EXTRACTION_NAME], indirect=True
-)
-async def test_afind_benchmark(setup_benchmark, setup_test_data):
-    res = await UserModelWithoutTTL.afind()
-    assert len(res) == len(setup_test_data)
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "setup_benchmark", [COMPARE_FULL_EXTRACTION_NAME], indirect=True
-)
-async def test_afind_keys_with_gather_benchmark(setup_benchmark, setup_test_data):
-    keys = await UserModelWithoutTTL.afind_keys()
-    res = await asyncio.gather(*[UserModelWithoutTTL.get(key) for key in keys])
-    assert len(res) == len(setup_test_data)
+@pytest.mark.parametrize("extract_method", [afind_extract, extract_each_key])
+async def test_compare_full_extraction(benchmark, setup_test_data, extract_method):
+    await benchmark(extract_method, UserModelWithoutTTL)
