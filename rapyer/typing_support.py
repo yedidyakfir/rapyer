83274b93e50aa7c833b129117660794552f8cf55
YedidyaHKfir <Yedidya@dono.ai>
2025-11-26 13:40:55 +0200

    [support-python310] - remove unused type

--- rapyer/typing_support.py
+++ rapyer/typing_support.py
@@ -1,14 +1,7 @@
-from typing import Callable
-
-from rapyer.types.base import RedisType
-
-TypeTransformer = Callable[[type], type[RedisType]]
-
-
 # For python 3.10 support
 try:
     from typing import Self
 except ImportError:
     from typing_extensions import Self
 
-__all__ = ["TypeTransformer", "Self"]
+__all__ = ["Self"]
