971535ffe4d2dea85359186283f6c5ff73aec509
YedidyaHKfir <Yedidya@dono.ai>
2025-11-30 17:18:20 +0200

    [add-buld-insert] - ainsert is adding from args

--- rapyer/base.py
+++ rapyer/base.py
@@ -4,7 +4,7 @@ import contextlib
 import functools
 import pickle
 import uuid
-from typing import ClassVar, Any, AsyncGenerator
+from typing import ClassVar, Any, AsyncGenerator, Unpack
 
 from pydantic import (
     BaseModel,
@@ -294,7 +294,7 @@ class AtomicRedisModel(BaseModel):
         return await cls.Meta.redis.keys(f"{cls.class_key_initials()}:*")
 
     @classmethod
-    async def ainert(cls, models: list[Self]):
+    async def ainert(cls, *models: Unpack[Self]):
         pipe = cls.Meta.redis.pipeline()
         for model in models:
             pipe.json().set(model.key, model.json_path, model.redis_dump())
