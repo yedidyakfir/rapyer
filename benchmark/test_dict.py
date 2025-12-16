e1fe38a7a58c1e8ef8a4590502b1fcb04b8f70fd
YedidyaHKfir <Yedidya@dono.ai>
2025-12-16 21:34:42 +0200

    [add-benchmarking] - some changes to test, to run in the same loop

--- benchmark/test_dict.py
+++ benchmark/test_dict.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import pytest
 
 from tests.models.collection_types import StrDictModel
@@ -9,9 +8,11 @@ from tests.models.collection_types import StrDictModel
 def test__dict_apop__sanity(benchmark, real_redis_client):
     # Arrange
     model = StrDictModel(metadata={"key": "value"})
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(model.asave())
 
     def run_sync():
-        return asyncio.run(model.metadata.apop()())
+        return loop.run_until_complete(model.metadata.apop("key"))
 
     result = benchmark(run_sync)
     assert result == "value"
