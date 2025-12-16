577f79c950128c1168b0f3bbb757b729aff22a60
YedidyaHKfir <Yedidya@dono.ai>
2025-12-16 23:22:08 +0200

    [add-benchmarking] - fix setup

--- benchmark/test_dict.py
+++ benchmark/test_dict.py
@@ -7,13 +7,14 @@ from tests.models.collection_types import StrDictModel
 @pytest.mark.benchmark
 def test__dict_apop__sanity(benchmark, real_redis_client):
     # Arrange
-    model = StrDictModel(metadata={"key": "value"})
     loop = asyncio.get_event_loop()
 
     def setup():
+        model = StrDictModel(metadata={"key": "value"})
         loop.run_until_complete(model.asave())
+        return (model,), {}
 
-    def run_sync():
+    def run_sync(model):
         return loop.run_until_complete(model.metadata.apop("key"))
 
     # Act
