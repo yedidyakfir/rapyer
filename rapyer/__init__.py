54ad96db7d907c3d8402eafe1dac049317b365f8
yedidyakfir <yedidyakfir@gmail.com>
2026-01-01 00:20:56 +0200

    fix bug in rapyer.get

--- rapyer/__init__.py
+++ rapyer/__init__.py
@@ -1,6 +1,6 @@
 """Redis Pydantic - Pydantic models with Redis as the backend."""
 
-from rapyer.base import AtomicRedisModel, aget, find_redis_models, ainsert
+from rapyer.base import AtomicRedisModel, aget, get, find_redis_models, ainsert
 from rapyer.init import init_rapyer, teardown_rapyer
 
 __all__ = [
